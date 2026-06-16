f52f39490fe19bca72ee7456ae7d94e209e3829a
Darkgrey_ <backupusa190@gmail.com>
2026-06-15 22:05:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,8 +33,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const titulo = document.getElementById('titleCard');
     titulo.addEventListener('click', () => {
-        const sfx = new Audio('./audios/splat.mp3');
-        sfx.play();
+        const splat = new Audio('./audios/splat.mp3');
+        splat.preload = 'auto';
+        splat.volume = 0.3;
+        playSound(splat);
     });
 
     const audio = document.getElementById('audio');
@@ -75,86 +77,100 @@ function createBubble() {
 
 setInterval(createBubble, 600);
 
-var sfx = new Audio('./audios/weirdroutejingle.mp3');
-var sfx2 = new Audio('./audios/savepoint.mp3');
-sfx.preload = 'auto';
+var weird = new Audio('./audios/weirdroutejingle.mp3');
+var save = new Audio('./audios/savepoint.mp3');
+weird.preload = 'auto';
+save.preload = 'auto';
+
+weird.volume = 0.2;
+save.volume = 0.2;
+
+function playSound(audio) {
+    audio.currentTime = 0;
+    audio.play();
+}
+
+/*
+ * playSound(weird); - Weird Route;
+ * playSound(save); - Save Point;
+*/
 
 Mousetrap.bind('h', function() {
-    sfx2.play();
+    playSound(save);
     alert("Going to Home tab.");
     showTab('home');
 })
 
 Mousetrap.bind('p', function() {
-    sfx2.play();
+    playSound(save);
     alert("Going to Projects tab.");
     showTab('projects');
 })
 
 Mousetrap.bind('c', function() {
-    sfx2.play();
+    playSound(save);
     alert("Going to Comission tab.");
     showTab('comission');
 })
 
 Mousetrap.bind('l', function() {
-    sfx2.play();
+    playSound(save);
     alert("Going to Links tab.");
     showTab('links');
 })
 
 Mousetrap.bind('up up down down left right left right b a', function() {
-    sfx.play();
+    playSound(weird);
     alert("Konami code! So you know your way around..");
 });
 
 Mousetrap.bind('z o n i a', function() {
-    sfx.play();
+    playSound(weird);
     alert("I serve only Darkon.")
 })
 
 Mousetrap.bind('d a r k g r e y', function() {
-    sfx.play();
+    playSound(weird);
     alert("That's me. What were you thinking?")
 })
 
 Mousetrap.bind('s n y w y', function() {
-    sfx.play();
+    playSound(weird);
     alert("Green giant! And.. nerd.");
 })
 
 Mousetrap.bind('p r o t o', function() {
-    sfx.play();
+    playSound(weird);
     alert("White dragon that is dumb.");
 })
 
 Mousetrap.bind('d a r k o n', function() {
-    sfx.play();
+    playSound(weird);
     alert("Well, it is me.. what do you want?");
 })
 
 Mousetrap.bind('r o g y', function() {
-    sfx.play();
+    playSound(weird);
     alert("I will headbutt you if you annoy me again!!");
 })
 
 Mousetrap.bind('n e b u l o n', function() {
-    sfx.play();
+    playSound(weird);
     alert("*happy plushie sounds*");
 })
 
 Mousetrap.bind('r y g a r', function() {
-    sfx.play();
+    playSound(weird);
     alert("Aah! Oh, i-it's you..! Sorry...");
 })
 
 Mousetrap.bind('d e e r', function() {
-    sfx.play();
+    playSound(weird);
     alert("I'll punch on your big stupid face if you do that again!");
 })
 
 Mousetrap.bind('b s k y', function() {
-    sfx2.play();
+    playSound(sfx2);
     alert("So you wanna see my posts? Cool.");
     window.open('https://darkgreyrai.bsky.social')
 })
